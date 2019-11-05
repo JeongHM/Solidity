6eacfce71f8154520373e539e447cf1848cee933
정해민(Min) <min.jeong@goodoc.co.kr>
2019-11-05 23:15:44 +0900

    [chore] 솔리디티 version 변경 0.4.19 -> 0.5.11

---
+++ CryptoZombie/Chapter_01/lesson_01.sol
@@ -0,0 +1,21 @@
+// Contract
+// Ethereum 애플리케이션의 기본적인 구성 요소
+
+// Version Pragma
+// 모든 솔리디티 소스 코드는 "version pragma"로 시작해야 하는데, 이는 해당 코드가 이용해야 하는 솔리디티 버전을 선언하는 것.
+
+// Version Pragma Example
+pragma solidity ^0.5.11;
+
+// contract Example
+contract HelloWorld {
+
+}
+
+// 우리의 좀비 군대 생성을 시작하기 위해 ZombieFactory라는 기본 컨트랙트를 생성해 보세!
+// 우측 박스에 우리 컨트랙트가 솔리디티 버전 0.4.19를 쓸 수 있도록 설정한다.
+// ZombieFactory라는 빈 컨트랙트를 생성한다.
+
+pragma solidity ^0.5.11;
+contract ZombieFactory {
+}
